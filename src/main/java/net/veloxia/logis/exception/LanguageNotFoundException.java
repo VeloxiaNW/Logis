cb2a934ea27c98f17281cb3173b2048371754f18
xRookieFight <me@xrookiefight.com>
2026-03-23 15:52:44 +0300

    chore: Change package name

--- src/main/java/net/veloxia/logis/exception/LanguageNotFoundException.java
+++ src/main/java/net/veloxia/logis/exception/LanguageNotFoundException.java
@@ -3,7 +3,6 @@ package net.veloxia.logis.exception;
 /**
  * Exception thrown when a language JSON file cannot be found at the given path.
  *
- * @param filePath The path to the missing language file
  * @author xRookieFight
  * @since 23/03/2026
  */
